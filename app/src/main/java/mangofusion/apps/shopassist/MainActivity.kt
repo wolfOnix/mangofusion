5f0fef4a3ddd159af3719cb71a32e7c88fa676cb
Kovats Robert <robertrobertzicku@yahoo.com>
2021-03-15 12:58:42 +0200

    SignUp Activity added. Link from MainActivity.kt to SignUp.kt. En (GB) Locale added. Kotlin versions updated

--- app/src/main/java/mangofusion/apps/shopassist/MainActivity.kt
+++ app/src/main/java/mangofusion/apps/shopassist/MainActivity.kt
@@ -1,11 +1,20 @@
 package mangofusion.apps.shopassist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /* Called when the user taps the Send button */
+    fun goToSignUp(view: View) {
+        val intent = Intent(this, SignUp::class.java).apply { }
+        startActivity(intent)
     }
 }
