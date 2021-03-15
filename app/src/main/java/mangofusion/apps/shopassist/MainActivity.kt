fe142755c334b40a80fe6947c804ad54b2c5fa6d
Kovats Robert <robertrobertzicku@yahoo.com>
2021-03-15 18:38:47 +0200

    Firebase test

--- app/src/main/java/mangofusion/apps/shopassist/MainActivity.kt
+++ app/src/main/java/mangofusion/apps/shopassist/MainActivity.kt
@@ -1,15 +1,19 @@
 package mangofusion.apps.shopassist
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("test")
+        myRef.setValue("Hello, World!")
     }
 
     /* Called when the user taps the Send button */
