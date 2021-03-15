773a3938c3326012569977c6c7a5293677c56621
Rus-Laviniu-Marius <ruslaviniumarius@gmail.com>
2021-03-15 18:57:15 +0200

    Updated Log In feature

--- app/src/main/java/mangofusion/apps/shopassist/MainActivity.kt
+++ app/src/main/java/mangofusion/apps/shopassist/MainActivity.kt
@@ -3,13 +3,17 @@ package mangofusion.apps.shopassist
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("Username")
+        myRef.setValue("Laviniu")
     }
 
     /* Called when the user taps the Send button */
@@ -17,4 +21,12 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, SignUp::class.java).apply { }
         startActivity(intent)
     }
+
+    /* Called when the user taps the Sign In button
+    fun goToLogIn(view: View) {
+        val intent = Intent(this, LogIn::class.java).apply { }
+        startActivity(intent)
+    }
+    */
+     
 }
