a4a11c6f756b64d2455c1a6a4ccc7f672adbf132
Kovats Robert <robertrobertzicku@yahoo.com>
2021-04-12 17:54:58 +0300

    ShoppingList and ShoppingListElement classes created

---
+++ app/src/main/java/mangofusion/apps/shopassist/ShoppingList.kt
@@ -0,0 +1,10 @@
+package mangofusion.apps.shopassist
+
+import java.time.LocalDateTime
+import java.util.*
+
+class ShoppingList(private var elementArray: Array<ShoppingListElement>) {
+
+    private var dateCreated: Calendar = Calendar.getInstance()
+
+}
---
+++ app/src/main/java/mangofusion/apps/shopassist/ShoppingListElement.kt
@@ -0,0 +1,9 @@
+package mangofusion.apps.shopassist
+
+class ShoppingListElement(
+    private var elementName: String,
+    private var quantity: Int,
+    private var unitOfMeasure: String
+) {
+
+}
