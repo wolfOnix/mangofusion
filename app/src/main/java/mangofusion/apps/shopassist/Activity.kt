957744b47e402bcac8b7e9961dce1373bbb6a120
Kovats Robert <robertrobertzicku@yahoo.com>
2021-05-03 13:35:17 +0300

    My Account activity now displays personal data of the current user

--- app/src/main/java/mangofusion/apps/shopassist/Activity.kt
+++ app/src/main/java/mangofusion/apps/shopassist/Activity.kt
@@ -1,9 +1,34 @@
 package mangofusion.apps.shopassist
 
-import android.util.Base64
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import javax.crypto.Cipher
-import javax.crypto.spec.IvParameterSpec
-import javax.crypto.spec.SecretKeySpec
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
-open class Activity : AppCompatActivity()
+open class Activity : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
+    var mDatabase: DatabaseReference = FirebaseDatabase.getInstance().reference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mAuth = FirebaseAuth.getInstance()
+        if (mAuth!!.currentUser == null) { // if user is not signed in, go to MainActivity
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
+    fun getUserID(): String {
+        val currUser: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+        return currUser?.uid ?: ""
+    }
+
+    fun signOut(currActivity: AppCompatActivity) {
+        FirebaseAuth.getInstance().signOut()
+        startActivity(Intent(currActivity, MainActivity::class.java))
+    }
+}
