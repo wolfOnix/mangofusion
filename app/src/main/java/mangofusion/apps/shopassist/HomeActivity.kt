8c3985a37467b57ed7bea2c60459f371279d3458
Kovats Robert <robertrobertzicku@yahoo.com>
2021-05-20 15:21:47 +0300

    Included xml files updated in order to cover a wider applicability

--- app/src/main/java/mangofusion/apps/shopassist/HomeActivity.kt
+++ app/src/main/java/mangofusion/apps/shopassist/HomeActivity.kt
@@ -12,7 +12,6 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
-
 class HomeActivity: Activity(), View.OnClickListener {
 
     private var btnHome: ImageButton? = null
@@ -62,7 +61,7 @@ class HomeActivity: Activity(), View.OnClickListener {
         if (takenIssuerUser == null || takenShoppingList == null)
             loadShoppingRequests()
         else
-            displayRequest(takenShoppingList!!, 3, 0)
+            displayCard(takenShoppingList!!, 3, 0)
     }
 
     private fun greet() {
@@ -82,7 +81,9 @@ class HomeActivity: Activity(), View.OnClickListener {
                     if (shList != null && !shList.delivered) { // the shopping list is still in request
                         if (shList.providerID == getUserID()) { // the current user is the provider of the shopping list -> no other shopping list will be displayed
                             USER_AS_PROVIDER = true
-                            displayRequest(shList, 3, 0)
+                            USER_AS_ISSUER = false // the provider role has a greater priority
+                            displayCard(shList, 3, 0)
+                            adaptLayout(3)
                             listOfShLists.clear()
                             break
                         }
@@ -93,10 +94,7 @@ class HomeActivity: Activity(), View.OnClickListener {
                         listOfShLists.add(shList)
                     }
                 }
-                if (USER_AS_PROVIDER) {
-                    println("CP0")
-                    USER_AS_ISSUER = false // the provider role has a greater priority, in case of conflicts caused by eventual future app design
-                } else if(listOfShLists.size > 0) { // there are lists to display
+                if(listOfShLists.size > 0) { // there are lists to display
                     println("CP1")
                     loopRequests(listOfShLists.toTypedArray())
                 } else if (listOfShLists.size == 0) { // there is no list to display
@@ -109,7 +107,15 @@ class HomeActivity: Activity(), View.OnClickListener {
             override fun onCancelled(databaseError: DatabaseError) {}
         }
         ref.addListenerForSingleValueEvent(valueEventListener)
+    }
 
+    private fun adaptLayout(mode: Int) {
+        when (mode) {
+            3 -> {
+                findViewById<LinearLayout>(R.id.lnly_requests_nr).visibility = View.GONE
+                findViewById<LinearLayout>(R.id.lnly_alternative_phrase).visibility = View.VISIBLE
+            }
+        }
     }
 
     private fun loopRequests(shListArr: Array<ShoppingList>) {
@@ -118,9 +124,9 @@ class HomeActivity: Activity(), View.OnClickListener {
             for (i in shListArr.indices) {
                 if (shListArr[i].issuerID == getUserID()) {
                     if (shListArr[i].taken)
-                        displayRequest(shListArr[i], 2, i)
+                        displayCard(shListArr[i], 2, i)
                     else
-                        displayRequest(shListArr[i], 1, i)
+                        displayCard(shListArr[i], 1, i)
                 }
             }
         } else if (!USER_AS_PROVIDER) { // user has no role
@@ -130,81 +136,73 @@ class HomeActivity: Activity(), View.OnClickListener {
             txvwRequestsNr?.text = nrNoRoleLists.toString()
 
             for (i in 0 until nrNoRoleLists) {
-                displayRequest(shListArr[i], 0, i)
+                displayCard(shListArr[i], 0, i)
             }
         }
     }
 
-    private fun displayRequest(shList: ShoppingList, mode: Int, order: Int): Boolean {
+    private fun displayCard(shList: ShoppingList, mode: Int, order: Int): Boolean {
         /* mode:
         0 - user - no role
         1 - user - issuer, shList - not taken
         2 - user - issuer, shList - taken
         3 - user - provider */
 
-            // inflate with shoppingListContainer
-            val v: View = layoutInflater.inflate(R.layout.shopping_list_ready_container_home, shoppingListsReadyContainer, false)
-            shoppingListsReadyContainer?.addView(v, order)
+        // inflate with shoppingListContainer
+        val v: View = layoutInflater.inflate(R.layout.shopping_list_ready_container_home, shoppingListsReadyContainer, false)
+        shoppingListsReadyContainer?.addView(v, order)
 
-            val oneListReadyContainer: ViewGroup = v as ViewGroup // this is the view which was added
+        val oneListReadyContainer: ViewGroup = v as ViewGroup // this is the view which was added
 
-            // fill the shoppingListContainer
-            val sz: Int = shList.elementArray.size
+        // fill the shoppingListContainer
+        val sz: Int = shList.elementArray.size
 
-            val txvwArtNr: TextView = oneListReadyContainer.getChildAt(1) as TextView
-            txvwArtNr.text = resources.getQuantityString(R.plurals.number_of_articles, sz, sz)
+        val txvwArtNr: TextView = oneListReadyContainer.getChildAt(1) as TextView
+        txvwArtNr.text = resources.getQuantityString(R.plurals.number_of_articles, sz, sz)
 
-            val elementsReadyContainer: ViewGroup = oneListReadyContainer.getChildAt(2) as ViewGroup
-            val txvwSubtitleNotes: TextView = oneListReadyContainer.getChildAt(3) as TextView
-            val txvwNotesContent: TextView = oneListReadyContainer.getChildAt(4) as TextView
-            val txvwAddress: TextView = oneListReadyContainer.getChildAt(6) as TextView
+        val elementsReadyContainer: ViewGroup = oneListReadyContainer.getChildAt(2) as ViewGroup
+        val txvwSubtitleNotes: TextView = oneListReadyContainer.getChildAt(3) as TextView
+        val txvwNotesContent: TextView = oneListReadyContainer.getChildAt(4) as TextView
+        val txvwAddress: TextView = oneListReadyContainer.getChildAt(6) as TextView
 
-            val btnOpenList: Button = oneListReadyContainer.getChildAt(7) as Button
-            btnOpenList.setOnClickListener { openList(order) }
+        val btnOpenList: Button = oneListReadyContainer.getChildAt(7) as Button
+        btnOpenList.setOnClickListener { openList(order) }
 
-            if (mode == 3) // remove btnOpenList
-                oneListReadyContainer.removeViewAt(7)
+        if (mode == 3) // remove btnOpenList
+            oneListReadyContainer.removeViewAt(7)
 
-            if (shList.observations.isEmpty()) {
-                txvwSubtitleNotes.visibility = View.GONE
-                txvwNotesContent.visibility = View.GONE
-            } else {
-                txvwNotesContent.text = shList.observations
-            }
+        if (shList.observations.isEmpty()) {
+            txvwSubtitleNotes.visibility = View.GONE
+            txvwNotesContent.visibility = View.GONE
+        } else {
+            txvwNotesContent.text = shList.observations
+        }
 
-            if (takenIssuerUser == null) { // if the issuer is not allocated -> the user is certainly not a provider
-                mDatabase.child("users").child(shList.issuerID).get().addOnSuccessListener {
-                    val issuerUser: User? = it.getValue(User::class.java)
-                    if (issuerUser != null) txvwAddress.text = "${issuerUser.city}, ${issuerUser.streetAndNumber}"
-                    if (issuerUser != null) {
-                        listOfIssuers.add(issuerUser)
-                    }
-                }.addOnFailureListener { }
-            } else if (mode == 3) {
-                txvwAddress.text = "${takenIssuerUser!!.city}, ${takenIssuerUser!!.streetAndNumber}"
-            }
-
-            for (i in 0 until sz) { // fill in all the elements in the list
-                val v: View = layoutInflater.inflate(R.layout.shopping_list_element, elementsReadyContainer, false)
-                elementsReadyContainer.addView(v, i)
-
-                if (mode == 3) { // make checkbox tickable
-                    ((elementsReadyContainer.getChildAt(i) as ViewGroup).getChildAt(0) as CheckBox).isClickable = true
+        if (takenIssuerUser == null) { // if the issuer is not allocated -> the user is certainly not a provider
+            mDatabase.child("users").child(shList.issuerID).get().addOnSuccessListener {
+                val issuerUser: User? = it.getValue(User::class.java)
+                if (issuerUser != null) txvwAddress.text = "${issuerUser.city}, ${issuerUser.streetAndNumber}"
+                if (issuerUser != null) {
+                    listOfIssuers.add(issuerUser)
                 }
+            }.addOnFailureListener { }
+        } else if (mode == 3) {
+            txvwAddress.text = "${takenIssuerUser!!.city}, ${takenIssuerUser!!.streetAndNumber}"
+        }
 
-                val txvwArtName: TextView = (elementsReadyContainer.getChildAt(i) as ViewGroup).getChildAt(1) as TextView? ?: return false
-                val txvwQuantAndUnit: TextView = (elementsReadyContainer.getChildAt(i) as ViewGroup).getChildAt(2) as TextView? ?: return false
+        for (i in 0 until sz) { // fill in all the elements in the list
+            val v: View = layoutInflater.inflate(R.layout.shopping_list_element, elementsReadyContainer, false)
+            elementsReadyContainer.addView(v, i)
 
-                txvwArtName.text = shList.elementArray[i].elementName
-                txvwQuantAndUnit.text = "${shList.elementArray[i].quantity} ${shList.elementArray[i].unitOfMeasure}"
+            if (mode == 3) { // make checkbox tickable
+                ((elementsReadyContainer.getChildAt(i) as ViewGroup).getChildAt(0) as CheckBox).isClickable = true
             }
 
-        when (mode) {
-            3 -> {
-                findViewById<LinearLayout>(R.id.lnly_requests_nr).visibility = View.GONE
-                findViewById<LinearLayout>(R.id.lnly_alternative_phrase).visibility = View.VISIBLE
+            val txvwArtName: TextView = (elementsReadyContainer.getChildAt(i) as ViewGroup).getChildAt(1) as TextView? ?: return false
+            val txvwQuantAndUnit: TextView = (elementsReadyContainer.getChildAt(i) as ViewGroup).getChildAt(2) as TextView? ?: return false
 
-            }
+            txvwArtName.text = shList.elementArray[i].elementName
+            txvwQuantAndUnit.text = "${shList.elementArray[i].quantity} ${shList.elementArray[i].unitOfMeasure}"
         }
 
         /*when (mode) {
